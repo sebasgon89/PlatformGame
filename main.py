20bfd0b146d91067bed0e9fcadc5cf80515e6cd7
sebasgon89 <seba3789@gmail.com>
2023-03-26 16:28:04 -0300

    I've fuckin' added the repo v2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # https://coderslegacy.com/python/pygame-platformer-game-development/
+# https://stackoverflow.com/questions/23401652/fatal-the-current-branch-master-has-no-upstream-branch
 
 import pygame
 from pygame.locals import *
